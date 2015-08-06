363fef19092819feec78123564c57ae1fb92a599
Stanley Chiang <mraaa@bu.edu>
2015-08-06 14:38:56 -0400

    Add Upload Function

--- php/upload.php
+++ php/upload.php
@@ -3,7 +3,8 @@ include("../rootpath.php");
 
 header('Content-Type: application/json');
 
-$output_dir = $pageroot . DS . "upload";
+// $output_dir = $pageroot . DS . "upload";
+$output_dir = "../upload";
 
 if(isset($_FILES["upload_file"]))
 {
@@ -19,36 +20,40 @@ if(isset($_FILES["upload_file"]))
 		$error =$_FILES["upload_file"]["error"];
 		//You need to handle  both cases
 		//If Any browser does not support serializing of multiple files using FormData() 
-		$firstName = $_POST['firstname'];
-		$lastName = $_POST['lastname'];
-		$email = $_POST['email'];
-		$type = $_POST['type'];
+		// $firstName = $_POST['firstname'];
+		// $lastName = $_POST['lastname'];
+		// $email = $_POST['email'];
+		// $type = $_POST['type'];
 
-		$output_dir = $output_dir . DS . $type;
+		// $output_dir = $output_dir . DS . $type;
 
-		if ($firstName == "" || $firstName == null) {
-			# code...
-			$firstName = "none";
-		}
-		if ($lastName == "" || $lastName == null) {
-			# code...
-			$lastName = "none";
-		}
-		$fileDir = $firstName . "_" . $lastName;
+		// if ($firstName == "" || $firstName == null) {
+		// 	# code...
+		// 	$firstName = "none";
+		// }
+		// if ($lastName == "" || $lastName == null) {
+		// 	# code...
+		// 	$lastName = "none";
+		// }
+		// $fileDir = $firstName . "_" . $lastName;
 	
-		if (file_exists($output_dir . DS . $fileDir) == false) {
-			mkdir($output_dir . DS . $fileDir);
-		}
+		// if (file_exists($output_dir . DS . $fileDir) == false) {
+		// 	mkdir($output_dir . DS . $fileDir);
+		// }
 	
 		if(!is_array($_FILES["upload_file"]["name"])) {	//single file
 			$fileName = $_FILES["upload_file"]["name"];
-		 	$fullFileName = $output_dir . DS . $fileDir . DS . time() . "_" . $fileName;
-		 	$fullContactName = $output_dir . DS . $fileDir . DS . "contact.txt";
+		 	// $fullFileName = $output_dir . DS . $fileDir . DS . time() . "_" . $fileName;
+		 	$fullFileName = $output_dir . DS . $fileName;
+		 	if (file_exists($fullFileName)) {
+		 		rename($fullFileName, $fullFileName . "_" . time());
+		 	}
+		 	// $fullContactName = $output_dir . DS . $fileDir . DS . "contact.txt";
 			if ($error == 0) {
 				# code...
 		 	 	//var_dump($fileName);
 		 		move_uploaded_file($_FILES["upload_file"]["tmp_name"], $fullFileName);
-		 		file_put_contents($fullContactName, $email);
+		 		// file_put_contents($fullContactName, $email);
 
 		    	$ret[]= $fileName;
 			} else {
@@ -56,20 +61,24 @@ if(isset($_FILES["upload_file"]))
 			}
 		} else { //Multiple files, file[]
 			$fileCount = count($_FILES["upload_file"]["name"]);
-		 	$fullContactName = $output_dir . DS . $fileDir . DS . "contact.txt";
+		 	// $fullContactName = $output_dir . DS . $fileDir . DS . "contact.txt";
 			for($i=0; $i < $fileCount; $i++) {
 				$fileName = $_FILES["upload_file"]["name"][$i];
-			 	$fullFileName = $output_dir . DS . $fileDir . DS . time() . "_" . $fileName;
-				if ($error[$i] == 0) {
+			 	// $fullFileName = $output_dir . DS . $fileDir . DS . time() . "_" . $fileName;
+				$fullFileName = $output_dir . DS . $fileName;
+			 	if (file_exists($fullFileName)) {
+			 		rename($fullFileName, $fullFileName . "_" . time());
+			 	}
+		 		if ($error[$i] == 0) {
 					# code...
 					//var_dump($fileName);
-					move_uploaded_file($_FILES["upload_file"]["tmp_name"][$i],$output_dir . DS . $fileDir . DS . time() . "_" . $fileName);
+					move_uploaded_file($_FILES["upload_file"]["tmp_name"][$i], $fullFileName);
 					$ret[]= $fileName;
 				} else {
 					$reterror = $reterror . $fileName . "-" . codeToMessage($error[$i]) . "\n";
 				}
 			}
-		 	file_put_contents($fullContactName, $email);
+		 	// file_put_contents($fullContactName, $email);
 			if (!($reterror == "")) {
 				# code...
 				throw new Exception($reterror, 999);
